76e608bc8dec40f20d0bacfe0321dd2134340618
Daniel <daniel.40.pompa@gmail.com>
2023-05-09 00:58:44 +0200

    Create AddCategory component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,36 @@
+import { useState } from 'react';
+import AddCategory from './components/AddCategory';
+
 function App() {
+  /* Create categories state.
+  It starts as an empty array and will be filled with what the user types in the form. */
+  const [categories, setCategories] = useState([]);
+
+  const handleAddCategory = newCategory => {
+    // Validate that categories are not repeated
+    const categoryExists = categories.find(element => {
+      return element.toLowerCase() === newCategory.toLowerCase();
+    });
+
+    if (categoryExists) return;
+
+    // Update the state of categories with the new category
+    setCategories([newCategory, ...categories]);
+  };
+
   return (
     <>
       <h1>Gif App</h1>
 
       {/* Form */}
+      <AddCategory onNewCategory={e => handleAddCategory(e)} />
 
       {/* Gifs List */}
+      <ol>
+        {categories.map(category => (
+          <li key={category}>{category}</li>
+        ))}
+      </ol>
     </>
   );
 }
---
+++ src/components/AddCategory.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+
+const AddCategory = ({ onNewCategory }) => {
+  const [inputValue, setInputValue] = useState('');
+
+  // Function to capture the value of what the user types in the form.
+  const handleInputChange = e => {
+    setInputValue(e.target.value);
+  };
+
+  /* Function that executes when the user submits the form.
+  Update the main state in App.jsx by adding the category */
+  const handleSubmit = e => {
+    e.preventDefault();
+    const newInputValue = inputValue.trim();
+    if (newInputValue.length <= 1) return;
+    onNewCategory(newInputValue); // Emit event to the parent component App.jsx
+    setInputValue('');
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type='text'
+        placeholder='Buscar gifs'
+        value={inputValue}
+        onChange={handleInputChange}
+      />
+    </form>
+  );
+};
+
+AddCategory.propTypes = {
+  onNewCategory: PropTypes.func,
+};
+
+export default AddCategory;
