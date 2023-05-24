64341fe9732ae1641cd8bf7be362b0970de645e8
Daniel <daniel.40.pompa@gmail.com>
2023-05-24 21:52:05 +0200

    Testing in the GifGrid component

---
+++ test/components/GifGrid.test.jsx
@@ -0,0 +1,45 @@
+import { render, screen } from '@testing-library/react';
+import GifGrid from '../../src/components/GifGrid';
+import { useFetchGifs } from '../../src/hooks/useFetchGifs';
+
+jest.mock('../../src/hooks/useFetchGifs');
+
+describe('Tests in <GrifGrid />', () => {
+  const category = 'Dragon Ball';
+
+  test('Should display initial loading', () => {
+    useFetchGifs.mockReturnValue({
+      images: [],
+      isLoading: true,
+    });
+
+    render(<GifGrid category={category} />);
+
+    expect(screen.getByText('Cargando...'));
+    expect(screen.getByText(category));
+  });
+
+  test('Should display items when images are loaded', () => {
+    const gifs = [
+      {
+        id: '135518c7-8685-4def-a97f-9c8b0e8919cf',
+        title: 'Goku',
+        url: 'https://localhost/goku.jpg',
+      },
+      {
+        id: '035660a1-7873-4d7d-9f63-e516c1f2b88f',
+        title: 'Vegeta',
+        url: 'https://localhost/vegeta.jpg',
+      },
+    ];
+
+    useFetchGifs.mockReturnValue({
+      images: gifs,
+      isLoading: false,
+    });
+
+    render(<GifGrid category={category} />);
+
+    expect(screen.getAllByRole('img').length).toBe(2);
+  });
+});
